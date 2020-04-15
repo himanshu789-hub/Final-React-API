048b28210e5015fd42098d63aa82787c49e1710b
himanshu789-hub <60208368+himanshu789-hub@users.noreply.github.com>
2020-04-15 21:47:59 +0530

    Add files via upload

--- API/DTOModel/UserDTO.cs
+++ API/DTOModel/UserDTO.cs
@@ -7,6 +7,7 @@ namespace CarPoolAPI.PostModel
 {
     public class UserDTO
     {
+        public int id{get;set;}
         public string name { get; set; }
         public string emailId{ get; set; }
         public string password { get; set; }
